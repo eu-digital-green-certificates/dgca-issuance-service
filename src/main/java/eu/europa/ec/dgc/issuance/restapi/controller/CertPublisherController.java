f6ca2c7d0b865c11a4a129003b5684222c5d4ceb
Artur T <81235055+a-trzewik@users.noreply.github.com>
2021-05-05 13:49:36 +0200

    Feat/dgcgconnector (#35)
    * update rest api to 1.3 spec
    
    * checkstyle
    
    * cert controller not only in dev profile
    
    * feat/dgc connector integration
    
    * Apply suggestions from code review
    
    * Update pom.xml
    
    Co-authored-by: Felix Dittrich <31076102+f11h@users.noreply.github.com>

---
+++ src/main/java/eu/europa/ec/dgc/issuance/restapi/controller/CertPublisherController.java
@@ -0,0 +1,35 @@
+package eu.europa.ec.dgc.issuance.restapi.controller;
+
+
+import eu.europa.ec.dgc.issuance.service.CertKeyPublisherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dgci/certPublish")
+@AllArgsConstructor
+public class CertPublisherController {
+    private final CertKeyPublisherService certKeyPublisherService;
+
+    @Operation(
+        summary = "publish edgc signing public key to dgc gateway",
+        description = "public key need to be published so the created edgc can be validated."
+            + " The signature is validated agains published keys"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "cert published")}
+    )
+    @PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> publishEdgcKeyToGateway() {
+        certKeyPublisherService.publishKey();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
